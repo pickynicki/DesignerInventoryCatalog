66c71bf3782e64ffaad627e9536b93a31816088a
Nicki M <nickim30@gmail.com>
2020-11-16 00:02:31 -0500

    first commit

---
+++ InsertData.sql
@@ -0,0 +1,105 @@
+﻿--Delete Table Data
+
+DELETE FROM [dbo].[TeamMembers];
+DELETE FROM [dbo].[Inventories];
+DELETE FROM [dbo].[Teams];
+DELETE FROM [dbo].[Products];
+DELETE FROM [dbo].[People];
+
+--Insert Data into People Table
+
+SET IDENTITY_INSERT dbo.People ON;
+
+INSERT INTO People (Id, FirstName, LastName, SignUpDate)
+VALUES (1, 'Jessica', 'Rabbit', '2017-06-21'),
+	   (2, 'Mandy', 'Patinkin', '2016-04-07'),
+	   (3, 'Brenda', 'Bee', '2015-03-11'),
+	   (4, 'David', 'Tennant', '2017-10-31'),
+	   (5, 'Christopher', 'Walken', '2018-09-18'),
+	   (6, 'Coral', 'Reef', '2017-06-21'),
+	   (7, 'Buffy', 'Summers', '2015-08-13'),
+	   (8, 'Princess', 'Leia', '2016-11-20'),
+	   (9, 'Hermione', 'Granger', '2020-07-22'),
+	   (10, 'Dorothy', 'Gale', '2014-02-28'),
+	   (11, 'Cry', 'Baby', '2019-1-20');
+
+SET IDENTITY_INSERT dbo.People OFF;
+
+--Insert Data into Teams Table
+
+SET IDENTITY_INSERT dbo.Teams ON;
+
+INSERT INTO Teams (Id, Name, TeamLeaderPersonId)
+VALUES (1, 'Princess', 8),
+	   (2, 'Pirate', 2),
+	   (3, 'Doctor', 4),
+	   (4, 'Muggle', 9),
+	   (5, 'CowBell', 5);
+
+SET IDENTITY_INSERT dbo.Teams OFF;
+
+--Insert Data into TeamMembers Table
+
+SET IDENTITY_INSERT dbo.TeamMembers ON;
+
+INSERT INTO TeamMembers (Id, PersonId, TeamId)
+VALUES (1, 1, 1),
+	   (2, 2, 2),
+	   (3, 3, 4),
+	   (4, 4, 3),
+	   (5, 5, 5),
+	   (6, 6, 2),
+	   (7, 7, 5),
+	   (8, 8, 1),
+	   (9, 9, 4),
+	   (10, 10, 3);
+
+SET IDENTITY_INSERT dbo.TeamMembers OFF;
+
+--Insert Data into Products Table
+
+SET IDENTITY_INSERT dbo.Products ON;
+
+INSERT INTO Products (Id, [Name], Size, Wholesale, MSRP)
+VALUES (1, 'Buzzz Transfer', 'A', 999, 1599),
+	   (2, 'Dread Pirate Transfer', 'C', 1799, 2199),
+	   (3, 'No Place Like Home Transfer', 'D', 2199, 2799),
+	   (4, 'Inconceivable Transfer', 'B', 1399, 1799),
+	   (5, 'More Cow Bell Transfer', 'A', 999, 1599),
+	   (6, 'Expelliarmous Transfer', 'B', 1399, 1799),
+	   (7, 'Finding Nemo Transfer', 'C', 1799, 2199),
+	   (8, 'Allonsy Transfer', 'A', 999, 1599),
+	   (9, 'I''m Not Bad Transfer', 'A', 999, 1599),
+	   (10, 'Love You Transfer', 'D', 2199, 2799 );
+
+SET IDENTITY_INSERT dbo.Products OFF;
+
+--Insert Data into Inventories Table
+
+SET IDENTITY_INSERT dbo.Inventories ON;
+
+INSERT INTO Inventories (Id, PersonId, ProductId, Quantity)
+VALUES (1, 1, 9, 2),
+	   (2, 1, 10, 1),
+	   (3, 2, 2, 2),
+	   (4, 2, 4, 1),
+	   (5, 3, 1, 2),
+	   (6, 3, 10, 1),
+	   (7, 4, 8, 3),
+	   (8, 4, 3, 1),
+	   (9, 5, 5, 5),
+	   (10, 5, 9, 1),
+	   (11, 6, 7, 2),
+	   (12, 6, 3, 1),
+	   (13, 6, 2, 1),
+	   (14, 7, 9, 3),
+	   (15, 7, 4, 1),
+	   (16, 8, 10, 3),
+	   (17, 8, 1, 1),
+	   (18, 9, 6, 3),
+	   (19, 9, 8, 1),
+	   (20, 10, 3, 4),
+	   (21, 10, 4, 1);
+
+SET IDENTITY_INSERT dbo.Inventories OFF;
+	   
---
+++ Queries.sql
@@ -0,0 +1,82 @@
+--Write a SELECT query that uses a WHERE clause
+
+SELECT * 
+FROM People
+WHERE FirstName = 'Mandy';
+
+--Write a  SELECT query that uses an OR and an AND operator
+
+
+SELECT *
+FROM People
+INNER JOIN TeamMembers
+ON TeamMembers.PersonId = People.Id
+INNER JOIN Inventories
+ON Inventories.PersonId = People.Id
+WHERE (TeamId = 3 OR TeamId = 4)
+AND ProductId = 8
+
+--Write a  SELECT query that filters NULL rows using IS NOT NULL
+
+SELECT *
+FROM Inventories
+WHERE Quantity IS NOT NULL;
+
+--Write a DML statement that UPDATEs a set of rows with a WHERE clause. The values used in the WHERE clause should be a variable
+
+DECLARE @price as int;
+SET @price = 999;
+
+UPDATE Products
+SET Wholesale = 1199
+WHERE Wholesale = @price;
+
+--Write a DML statement that DELETEs a set of rows with a WHERE clause. The values used in the WHERE clause should be a variable
+
+DECLARE @personid as int;
+SET @personid = 11;
+
+DELETE FROM People
+WHERE Id = @personid;
+
+--Write a DML statement that DELETEs rows from a table that another table references. This script will have to also DELETE any records that reference these rows. Both of the DELETE statements need to be wrapped in a single TRANSACTION.
+
+BEGIN TRANSACTION;
+
+DELETE FROM Inventories
+WHERE ProductId = 1;
+
+DELETE FROM Products
+WHERE Id = 1;
+
+COMMIT;
+
+--Write a  SELECT query that utilizes a JOIN
+
+SELECT People.*, Teams.Name as TeamName FROM Teams
+INNER JOIN People
+ON Teams.TeamLeaderPersonId = People.Id
+
+--Write a  SELECT query that utilizes a JOIN with 3 or more tables
+--
+--Write a  SELECT query that utilizes a LEFT JOIN
+--
+--Write a  SELECT query that utilizes a variable in the WHERE clause
+--
+--Write a  SELECT query that utilizes a ORDER BY clause
+--
+--Write a  SELECT query that utilizes a GROUP BY clause along with an aggregate function
+--
+--Write a SELECT query that utilizes a CALCULATED FIELD
+--
+--Write a SELECT query that utilizes a SUBQUERY
+--
+--Write a SELECT query that utilizes a JOIN, at least 2 OPERATORS (AND, OR, =, IN, BETWEEN, ETC) AND A GROUP BY clause with an aggregate function
+--
+--Write a SELECT query that utilizes a JOIN with 3 or more tables, at 2 OPERATORS (AND, OR, =, IN, BETWEEN, ETC), a GROUP BY clause with an aggregate function, and a HAVING clause
+--
+--Design a NONCLUSTERED INDEX with ONE KEY COLUMN that improves the performance of one of the above queries
+--
+--Design a NONCLUSTERED INDEX with TWO KEY COLUMNS that improves the performance of one of the above queries
+--
+--Design a NONCLUSTERED INDEX with AT LEAST ONE KEY COLUMN and AT LEAST ONE INCLUDED COLUMN that improves the performance of one of the above queries
